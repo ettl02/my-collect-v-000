cb31ef3fb0f6c9709faf9725ae10761a24f8a5fe
Emily Wessel <ettl02@gmail.com>
2018-06-24 14:51:29 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+collection []
 
-
+def my_collect (collection)
+  i = 0
+  while i < collection.length
+    yield (collection[i])
+    my_collect (collection) do |x|
+    x.collect (" ").first 
+  end 
+end 
